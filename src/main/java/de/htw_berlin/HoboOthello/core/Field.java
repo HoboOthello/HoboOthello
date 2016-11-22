177e16f217dbc8b30de4f3dd62aa2daa15d02260
Steffen Exler <Steffen.Exler@gmail.com>
2016-11-22 21:46:59 +0100

    update getter & setter and add an security feature that only one field state is possible at a time

--- src/main/java/de/htw_berlin/HoboOthello/core/Field.java
+++ src/main/java/de/htw_berlin/HoboOthello/core/Field.java
@@ -10,10 +10,55 @@ public class Field {
     private boolean white;
     private boolean black;
 
+    /**
+     * set an empty field
+     */
+    public Field() {
+        setEmpty();
+    }
+
     public Field(boolean empty, boolean white, boolean black) {
-        this.empty = empty;
-        this.white = white;
-        this.black = black;
+        int tmp = 0;
+
+        if (empty) {
+            tmp++;
+            setEmpty();
+        }
+
+        if (white) {
+            tmp++;
+            setWhite();
+        }
+
+        if (black) {
+            tmp++;
+            setBlack();
+        }
+
+        //todo show warning if (0 == tmp || tmp > 1)
+    }
+
+    /**
+     * Add an field with just one possible condition
+     * b == Black
+     * w == White
+     * everything else == empty
+     *
+     * @param FieldState b for Black field
+     *                   w for White field
+     *                   e for empty field
+     */
+    public Field(char FieldState) {
+        switch (FieldState) {
+            case 'b':
+                setBlack();
+                break;
+            case 'w':
+                setWhite();
+                break;
+            default:
+                setEmpty();
+        }
     }
 
     public boolean possibleForActualPlayer() {
@@ -23,29 +68,49 @@ public class Field {
     }
 
     //getter und setter schon mal bereitgestellt, werden noch nicht benutzt!
-    public boolean isEmpty() {
-        return empty;
+    public boolean getEmpty() {
+        return this.empty;
     }
 
-    public boolean isWhite() {
-        return white;
+    public boolean getWhite() {
+        return this.white;
     }
 
-    public boolean isBlack() {
-        return black;
+    public boolean getBlack() {
+        return this.black;
     }
 
-    public void setEmpty(boolean empty) {
-        this.empty = empty;
+    /**
+     * get the current field state as an String
+     *
+     * @return the current field state "empty", "white", "black"
+     */
+    @Override
+    public String toString() {
+        if (this.empty) {
+            return "empty";
+        } else if (this.white) {
+            return "white";
+        } else {
+            return "black";
+        }
     }
 
-    public void setWhite(boolean white) {
-        this.white = white;
+    public void setEmpty() {
+        this.empty = true;
+        this.white = false;
+        this.black = false;
     }
 
-    public void setBlack(boolean black) {
-        this.black = black;
+    public void setWhite() {
+        this.empty = false;
+        this.white = true;
+        this.black = false;
     }
 
-
+    public void setBlack() {
+        this.empty = false;
+        this.white = false;
+        this.black = true;
+    }
 }
