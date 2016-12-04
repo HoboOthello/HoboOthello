be7ec92db4eb49937274ccf8d9263a029878493d
Laura Hartgers <laurahartgers@live.nl>
2016-12-04 19:02:59 +0100

    add ki random pick and  level

---
+++ src/main/java/de/htw_berlin/HoboOthello/Core/Level.java
@@ -0,0 +1,8 @@
+package de.htw_berlin.HoboOthello.Core;
+
+/**
+ * Created by laura on 04.12.16.
+ */
+public enum Level {
+    LEVEL1, LEVEL2, LEVEL3
+}
--- src/main/java/de/htw_berlin/HoboOthello/KI/KI.java
+++ src/main/java/de/htw_berlin/HoboOthello/KI/KI.java
@@ -2,12 +2,15 @@ package de.htw_berlin.HoboOthello.KI;
 
 import de.htw_berlin.HoboOthello.Core.*;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
-public class KI extends Player{
+public class KI extends Player {
 
     private Color kiColor;
+    private Level level;
 
     private Board board;
     private Field field;
@@ -16,23 +19,29 @@ public class KI extends Player{
     public KI(Color kiColor) {
         this.kiColor = kiColor;
     }
-
+    //rückgabe an controller ist ein field
     public Field setMove(Board board) {
         //return Field fieldToSetMove;
         return null;
     }
 
-    private HashMap listPossibleMoves(){
-        HashMap listOfPossibleMoves = new HashMap();
-        int max = gameRule.getPossibleMoves();
-        if (gameRule.isMoveAllowed(field, kiColor)) {
+    private List<Field> listPossibleMoves() {
+        List<Field> listOfPossibleMoves = new ArrayList<Field>();
 
-            for (int i = 0; i >= max; i++) {
-                listOfPossibleMoves.put(i, this.field);
+        for (Field field : board.iterateThroughAllFields()) {
+            if (gameRule.isMoveAllowed(field, kiColor)) {
+                listOfPossibleMoves.add(this.field);
             }
+
         }
-        //return HashMap;
-        return null;
+        return listOfPossibleMoves;
+    }
+
+    private Field pickRandomFieldFromList(){
+        List<Field> listOfPossibleMoves = listPossibleMoves();
+        int randomNumber = (int )(Math.random() * listOfPossibleMoves.size() + 1); // picks random index of field in list
+        return listOfPossibleMoves.get(randomNumber);
+
     }
 
 }
