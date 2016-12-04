66a6a7f3903626e1cd94280c6fb4c0abf13b933e
Björn Uhlig <bjoernuhlig@gmail.com>
2016-12-04 14:16:10 +0100

    last commit

--- src/main/java/de/htw_berlin/HoboOthello/GUI/Gameview.java
+++ src/main/java/de/htw_berlin/HoboOthello/GUI/Gameview.java
@@ -1,107 +1,179 @@
 package de.htw_berlin.HoboOthello.GUI;
 
-import de.htw_berlin.HoboOthello.Core.Field;
+/**
+ * Gameview class
+ * methods
+ *
+ * createGameView()
+ * addButtonListener()
+ * displayErrorMessage()
+ * setBoard() - Groesse festlegen
+ */
+
+
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Created by laura on 24.11.16.
  */
 public class Gameview extends JFrame {
 
+
+    public Gameview(){
+        super();
+    }
+
     private JLabel whiteScore = new JLabel("WHITE");
     private JLabel blackScore = new JLabel("BLACK");
     private Color backgroundColor = new Color(0, 150, 0);
     private JButton startButton = new JButton();
-    private JButton passButton = new JButton();
+    private JButton fieldView[][];
 
 
-    private Field[][] fields;
+    /**
+     * the method to set up the board
+     * @param boardSize
+     *
+     */
+    //TODO create instance of method in controller
+    public void setBoard(int boardSize){
 
-    //TODO: moment mal, das ist eine Methode die wenn schon, dann in Fields gehört? was macht die hier?
-    //TODO gemeinsam überlegen.
-    public void setFields(Field[][] fields) {
-        this.fields = fields;
+        fieldView = new JButton[boardSize][boardSize];
+
     }
 
-    public void CreateGameView() {
 
-        //sets up the main frame of the game
-        JFrame mainFrame = new JFrame();
+    /**
+     * method to create the gui
+     */
 
-        mainFrame.setTitle("HoboOthello");
-        mainFrame.setLocation(800, 200);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(800, 800);
-        mainFrame.setLayout(new BorderLayout());
-        mainFrame.setVisible(true);
+    public void createGameView() {
 
+        /*
+         * sets up the main frame of the game
+         */
+        this.setTitle("HoboOthello");
+        this.setLocation(800, 200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,800);
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
 
-        //create a board
+        /*
+         * Menue
+         */
+        this.setJMenuBar( new JMenuBar() );
+        JMenu gameMenu = new JMenu ("Datei");
+        JMenuItem closeGame = new JMenuItem("Exit");
+        JMenuItem newGame = new JMenuItem("New Game");
+        gameMenu.add(newGame);
+        gameMenu.add(closeGame);
+        this.getJMenuBar().add(gameMenu);
+
+        /*
+         * create a board. board is the center panel
+         */
         JPanel boardPanel = new JPanel();
-        boardPanel.setLayout(new GridLayout(8, 8));
-        boardPanel.setPreferredSize(new Dimension(650, 650));
-        boardPanel.setBorder(BorderFactory.createEtchedBorder());
-
+        boardPanel.setLayout( new GridLayout(8,8) );
+        boardPanel.setBorder( BorderFactory.createEtchedBorder() );
 
 		/*
-         * create Fields out of the Field class and fill the board with fields
+		 * create Fields out of the Field class and fill the board with fields
 		 * field ist initialized with magic numbers but that will later be changed to a variable 'boardsize'
 		 */
-        //TODO adding a variable for the boardsize and fix the size of the gui for larger or smaller boards
+        //TODO add the double for-loop to the method setBoard()
 
-        for (int row = 0; row < this.fields.length; row++) {
-            for (int column = 0; column < this.fields.length; column++) {
-                // fields[row][column] = new Field();
-                // fields[row][column].setBackground(backgroundColor);
-                // fields[row][column].setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
-                // boardPanel.add(fields[row][column]).setVisible(true);
+
+        fieldView = new JButton[8][8];
+
+        for(int row=0;row<fieldView.length;row++){
+            for(int column=0;column<fieldView.length;column++){
+
+                fieldView[row][column] = new JButton();
+                fieldView[row][column].setBackground( backgroundColor );
+                fieldView[row][column].setBorder( BorderFactory.createLineBorder( Color.BLACK, 1) );
+                boardPanel.add( fieldView[row][column] ).setVisible( true );
 
             }
         }
 
 
-        //the panel for all action buttons
+        /*
+         * the action panel which holds the start button
+         */
         JPanel actionPanel = new JPanel();
-        actionPanel.setPreferredSize(new Dimension(800, 100));
-        actionPanel.setBorder(BorderFactory.createEtchedBorder());
+        actionPanel.setBorder (BorderFactory.createEtchedBorder());
         startButton.setText("Start");
-        startButton.setPreferredSize(new Dimension(80, 30));
+        startButton.setPreferredSize(new Dimension(80,30));
         actionPanel.add(startButton);
-        passButton.setText("Passen");
-        passButton.setPreferredSize(new Dimension(80, 30));
-        actionPanel.add(passButton);
 
-        //the panel to display the actual score
+
+        /*
+         * the score panel to display the actual score
+         */
         JPanel scorePanel = new JPanel(new FlowLayout());
-        scorePanel.setPreferredSize(new Dimension(800, 50));
-//		scorePanel.setBorder (BorderFactory.createEtchedBorder());
         whiteScore.setBorder(BorderFactory.createEtchedBorder());
         blackScore.setBorder(BorderFactory.createEtchedBorder());
         scorePanel.add(whiteScore);
         scorePanel.add(blackScore);
 
-        //filler space for the right hand side
-        JPanel eastPanel = new JPanel(new GridLayout());
-        eastPanel.setPreferredSize(new Dimension(75, 700));
-//		eastPanel.setBorder (BorderFactory.createEtchedBorder());
 
-        //filler pace for the left hand side
+        /*
+         * filler spaces for the right and left hand side
+         */
+        JPanel eastPanel = new JPanel();
         JPanel westPanel = new JPanel();
-        westPanel.setPreferredSize(new Dimension(75, 700));
-//		westPanel.setBorder (BorderFactory.createEtchedBorder());
 
-        //adding all elements to the main frame
-        mainFrame.getContentPane().add(scorePanel, "North");
-        mainFrame.getContentPane().add(boardPanel, "Center");
-        mainFrame.getContentPane().add(actionPanel, "South");
-        mainFrame.getContentPane().add(eastPanel, "East");
-        mainFrame.getContentPane().add(westPanel, "West");
-        mainFrame.setJMenuBar(new JMenuBar());
-        mainFrame.setVisible(true);
+
+        /*
+         * BorderLayout dimensions
+         */
+        actionPanel.setPreferredSize( new Dimension(0, 100) );
+        scorePanel.setPreferredSize( new Dimension(0, 50) );
+        westPanel.setPreferredSize( new Dimension(75, 0) );
+        eastPanel.setPreferredSize( new Dimension(75, 0) );
+
+
+        /*
+         * adding all elements to the main frame
+         */
+        this.getContentPane().add( scorePanel, BorderLayout.NORTH );
+        this.getContentPane().add( boardPanel, BorderLayout.CENTER );
+        this.getContentPane().add( actionPanel, BorderLayout.SOUTH );
+        this.getContentPane().add( eastPanel, BorderLayout.EAST );
+        this.getContentPane().add( westPanel, BorderLayout.WEST );
+        this.setJMenuBar( new JMenuBar() );
+        this.setVisible(true);
 
     }
 
+
+    /*
+     * the method to add an actionListener to the Gameview
+     */
+    //TODO add ActionListener to Controller
+    void addButtonListener(ActionListener listenerForFieldButton) {
+
+        for( int row=0; row<fieldView.length; row++ ) {
+            for( int column=0; column<fieldView.length; column++ ) {
+
+                fieldView[ row ][ column ].addActionListener( listenerForFieldButton );
+
+            }
+        }
+    }
+
+    /*
+     * error message to display errors
+     * e.g. wrong button clicked = illegal move
+     */
+    public void displayErrorMessage( String errorMessage ) {
+
+        JOptionPane.showMessageDialog(this, errorMessage);
+
+    }
 
 }
